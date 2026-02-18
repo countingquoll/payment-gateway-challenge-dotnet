544312066f17dd210dd3361b20405b4c9677872e
Mike Agg <mikeagg.uk@gmail.com>
2026-02-18 15:07:38 +0000

    Fix existing failing unit test

--- src/PaymentGateway.Api/Controllers/PaymentsController.cs
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -21,6 +21,9 @@ public class PaymentsController : Controller
     {
         var payment = _paymentsRepository.Get(id);
 
+        if (payment == null)
+            return NotFound();
+
         return new OkObjectResult(payment);
     }
 }
