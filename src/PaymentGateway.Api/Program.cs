ad891b05a46583d28c5b746fe37a21f8d0f349b2
Mike Agg <mikeagg.uk@gmail.com>
2026-02-19 16:47:45 +0000

    Add unit tests for validator

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<PostPaymentRequestValidator>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ test/PaymentGateway.Api.Tests/PostPaymentRequestValidatorTests.cs
@@ -0,0 +1,125 @@
+using FluentValidation.TestHelper;
+
+using PaymentGateway.Api.Models.Requests;
+using PaymentGateway.Api.Services;
+using PaymentGateway.Api.Validators;
+
+namespace PaymentGateway.Api.Tests;
+
+public class PostPaymentRequestValidatorTests
+{
+    private readonly PostPaymentRequestValidator _validator = new(new CurrencyService());
+
+    private static PostPaymentRequest ValidRequest() => new()
+    {
+        CardNumberLastFour = 1234,
+        ExpiryMonth = 6,
+        ExpiryYear = DateTime.UtcNow.Year + 1,
+        Currency = "GBP",
+        Amount = 100,
+        Cvv = 123
+    };
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(13)]
+    public void ShouldHaveErrorForInvalidExpiryMonth(int month)
+    {
+        var request = ValidRequest();
+        request.ExpiryMonth = month;
+
+        _validator.TestValidate(request)
+            .ShouldHaveValidationErrorFor(x => x.ExpiryMonth);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(12)]
+    public void ShouldNotHaveErrorForValidExpiryMonth(int month)
+    {
+        var request = ValidRequest();
+        request.ExpiryMonth = month;
+
+        _validator.TestValidate(request)
+            .ShouldNotHaveValidationErrorFor(x => x.ExpiryMonth);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void ShouldHaveErrorForInvalidAmount(int amount)
+    {
+        var request = ValidRequest();
+        request.Amount = amount;
+
+        _validator.TestValidate(request)
+            .ShouldHaveValidationErrorFor(x => x.Amount);
+    }
+
+    [Fact]
+    public void ShouldNotHaveErrorForAmountOfOne()
+    {
+        var request = ValidRequest();
+        request.Amount = 1;
+
+        _validator.TestValidate(request)
+            .ShouldNotHaveValidationErrorFor(x => x.Amount);
+    }
+
+    [Theory]
+    [InlineData("JPY")]
+    [InlineData("AUD")]
+    [InlineData("")]
+    public void ShouldHaveErrorForUnsupportedCurrency(string currency)
+    {
+        var request = ValidRequest();
+        request.Currency = currency;
+
+        _validator.TestValidate(request)
+            .ShouldHaveValidationErrorFor(x => x.Currency);
+    }
+
+    [Theory]
+    [InlineData("USD")]
+    [InlineData("EUR")]
+    [InlineData("GBP")]
+    public void ShouldNotHaveErrorForSupportedCurrency(string currency)
+    {
+        var request = ValidRequest();
+        request.Currency = currency;
+
+        _validator.TestValidate(request)
+            .ShouldNotHaveValidationErrorFor(x => x.Currency);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorForExpiredCard()
+    {
+        var now = DateTime.UtcNow;
+        var request = ValidRequest();
+        request.ExpiryMonth = now.Month;
+        request.ExpiryYear = now.Year - 1;
+
+        _validator.TestValidate(request)
+            .ShouldHaveValidationErrorFor(x => x.ExpiryMonth);
+    }
+
+    [Fact]
+    public void ShouldNotHaveErrorForCardExpiringThisMonth()
+    {
+        var now = DateTime.UtcNow;
+        var request = ValidRequest();
+        request.ExpiryMonth = now.Month;
+        request.ExpiryYear = now.Year;
+
+        _validator.TestValidate(request)
+            .ShouldNotHaveValidationErrorFor(x => x.ExpiryMonth);
+    }
+
+    [Fact]
+    public void ShouldNotHaveAnyErrorsForValidRequest()
+    {
+        _validator.TestValidate(ValidRequest())
+            .ShouldNotHaveAnyValidationErrors();
+    }
+}
